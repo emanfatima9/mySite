86e1798c39e4b25021ecdf741045a7b79c8fd7f5
Talha Anees <https://github.com/talhaanees/>
2018-05-29 06:30:38 +0300

    80% done

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
+
+const sqlite3 = require('sqlite3').verbose();
+var db = require('./sql/db_manage.js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../sql/db_manage.js");
+
+var multer = require('multer');
+
+
+var dt = new Date();
+var randomVal;
+
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      randomVal = Math.floor((Math.random() * 1000) + 1);
+      cb(null, dt.getMonth() + dt.getDate() + dt.getDay() + "-" + dt.getHours() + dt.getMinutes() + "-" + randomVal + (".jpg"));
+    }
+});
+var upload = multer({storage: storage});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +29,43 @@ router.get('/home', function(req, res, next) {
   res.render('home', {});
 });
 
+router.get('/addapost', function(req, res, next){
+  res.render('addpost', {message : ""});
+});
 
+router.post('/addPost', upload.single('uImg'), function(req, res, next){
+	try{	
+		var name = req.body.uName;
+		var nick = req.body.uNick;
+		var desc = req.body.uMessage;
+		var img = req.body.uImg;
+		console.log("randomVal: " + randomVal);
+		var imgin = dt.getMonth() + dt.getDate() + dt.getDay() + "-" + dt.getHours() + dt.getMinutes() + "-" + randomVal + (".jpg");
+
+		console.log (name);
+		console.log (imgin);
+
+		let sql = `INSERT INTO post (name, nick, description, image) VALUES(?,?,?,?)`;
+
+		db.run(sql, [name, nick, desc, imgin], function(err){
+			if (err){
+				return console.log("Insert Post Error: " + err.message);
+			}
+			console.log(name + ` added Successfully with rowid ${this.lastID}`);
+			var message = imgin + " created successfully.";
+			res.render('addpost', { message: message});
+		});
+	}
+	catch(ex){
+		console.error("Internal error:"+ex);
+		return next(ex);
+	}
+	
+});
+
+
+router.get('/viewallposts', function(req, res, next){
+	res.render('viewalloffers', {});
+});
 
 module.exports = router;
